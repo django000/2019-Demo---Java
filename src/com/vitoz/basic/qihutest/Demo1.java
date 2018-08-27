0a06fe71d77f7ae8cb8c30ce30a291c65509b8da
vitoz <zhangwentao0601@163.com>
2018-08-27 23:09:54 +0800

    新增360笔试题

---
+++ src/com/vitoz/basic/qihutest/Demo1.java
@@ -0,0 +1,31 @@
+package com.vitoz.basic.qihutest;
+
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int xMax = Integer.MIN_VALUE, xMin=Integer.MAX_VALUE;
+        int yMax = Integer.MIN_VALUE, yMin=Integer.MAX_VALUE;
+        int xi, yi, max;
+        for (int i=0;i<n;i++){
+            xi = sc.nextInt();
+            yi = sc.nextInt();
+            if (xi<xMin){
+               xMin = xi;
+            }
+            if (xi>xMax){
+                xMax = xi;
+            }
+            if (yi<yMin){
+                yMin = yi;
+            }
+            if (yi>yMax){
+                yMax = yi;
+            }
+        }
+        max = Math.max(yMax-yMin, xMax-xMin);
+        System.out.println(max*max);
+    }
+}
